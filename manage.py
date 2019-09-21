63723fdad8d8c83f6698549f132a5056896a297d
kamauvick <kamauvick@outlook.com>
2019-09-21 12:51:20 +0300

    Add the application driver file.
    Add flask_migrations and a shell context to the file.

---
+++ manage.py
@@ -0,0 +1,20 @@
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+from app import create_app, db
+from app.models import User
+
+app = create_app()
+
+manager = Manager(app)
+manager.add_command('server', Server(use_debugger=True))
+
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+@manager.shell
+def add_shell_context():
+    return {'db': db, 'User': User}
+
+
+if __name__ == '__main__':
+    manager.run()
