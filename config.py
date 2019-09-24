7e2c33e4679f015e9586a15e2c81773698110d20
kamauvick <kamauvick@outlook.com>
2019-09-24 12:15:42 +0300

    Add mail server configurations

--- config.py
+++ config.py
@@ -7,4 +7,13 @@ ld()
 
 class Config:
     debug = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://vick:p@127.0.0.1:5432/blogx'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
